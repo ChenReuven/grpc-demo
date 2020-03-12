99a2287e3db23cef47c43344c5f4e5d6b58a5242
Chen Reuven <Chen.Reuven@emc.com>
2020-03-12 15:23:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/hnasr/javascript_playground/tree/master/grpc-demo
---
+++ node-server/server.js
@@ -0,0 +1,45 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+const packageDef = protoLoader.loadSync("../proto/todo.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+
+const server = new grpc.Server();
+server.bind("0.0.0.0:40000",
+    grpc.ServerCredentials.createInsecure());
+
+server.addService(todoPackage.Todo.service,
+    {
+        "createTodo": createTodo,
+        "readTodos" : readTodos,
+        "readTodosStream": readTodosStream,
+        "deleteTodo": deleteTodo
+    });
+server.start();
+console.log(`gRPC Server Start on 0.0.0.0:40000`);
+
+
+const todos = []
+function createTodo (call, callback) {
+    const todoItem = {
+        "id": todos.length + 1,
+        "text": call.request.text
+    }
+    todos.push(todoItem)
+    callback(null, todoItem);
+}
+
+
+function readTodosStream(call, callback) {
+
+    todos.forEach(t => call.write(t));
+    call.end();
+}
+
+function readTodos(call, callback) {
+    callback(null, {"items": todos})
+}
+
+function deleteTodo(call, callback) {
+
+}
